248c73152965c46fe16090448b5fdf5ba9fe71db
stoneyang <stoneyang0915@gmail.com>
2018-02-08 15:48:35 +0800

    Formatting

--- src/caffe/layers/compress_conv_layer.cu
+++ src/caffe/layers/compress_conv_layer.cu
@@ -13,33 +13,41 @@ namespace caffe {
 // The constant NUM_THREADS should be equal to the value in CCMomentCalc
 template <typename Dtype>
 __global__ void CCMomentCollect(const int n, const Dtype* wb, const Dtype* mask,
-    Dtype* mu, Dtype* std, unsigned int* count ) {  
+    Dtype* mu, Dtype* std, unsigned int* count) {  
   const int NUM_THREADS = 512;  
   __shared__ Dtype param [4*NUM_THREADS]; 
   __shared__ unsigned int tcount [2*NUM_THREADS];   
   unsigned int t = threadIdx.x; 
   unsigned int s = 2 * blockIdx.x * NUM_THREADS;
-  if (s+t < n){
+  if (s+t < n) {
     param[t] = fabs(mask[s+t]*wb[s+t]);
     param[t+2*NUM_THREADS] = mask[s+t]*wb[s+t]*wb[s+t];
-    if(mask[s+t]*wb[s+t]!=0) tcount[t] = 1;
-    else tcount[t] = 0;
+    if(mask[s+t]*wb[s+t] != 0) {
+      tcount[t] = 1;
+    } else {
+      tcount[t] = 0;
+    }
+  } else {
+    param[t] = 0;
+    param[t+2*NUM_THREADS] = 0;
+    tcount[t] = 0;
   }
-  else{
-    param[t] = 0;param[t+2*NUM_THREADS] = 0;tcount[t] = 0;
-  }
-  if (s+t+NUM_THREADS < n){
+  if (s+t+NUM_THREADS < n) {
     param[t+NUM_THREADS] = fabs(mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS]);
     param[t+3*NUM_THREADS] = mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS];
-    if(mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS]!=0) tcount[t+NUM_THREADS] = 1;
-    else tcount[t+NUM_THREADS] = 0;
-  }
-  else{
-    param[t+NUM_THREADS] = 0;param[t+3*NUM_THREADS] = 0;tcount[t+NUM_THREADS] = 0;  
+    if (mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS] != 0) {
+      tcount[t+NUM_THREADS] = 1;
+    } else {
+      tcount[t+NUM_THREADS] = 0;
+    }
+  } else {
+    param[t+NUM_THREADS] = 0;
+    param[t+3*NUM_THREADS] = 0;
+    tcount[t+NUM_THREADS] = 0;  
   }
   __syncthreads(); 
   for(unsigned int stride = NUM_THREADS; stride >= 1; stride >>= 1) {
-    if (t < stride ){
+    if (t < stride ) {
       param[t] += param[t+stride]; 
       param[t+2*NUM_THREADS] += param[t+2*NUM_THREADS+stride];
       tcount[t] += tcount[t+stride];
@@ -55,27 +63,27 @@ __global__ void CCMomentCollect(const int n, const Dtype* wb, const Dtype* mask,
 
 // The constant NUM_THREADS should be equal to the value in CCMomentCalc
 template <typename Dtype>
-__global__ void CCNzeroCollect(const int n, const Dtype* mask, unsigned int* count ) {  
+__global__ void CCNzeroCollect(const int n, const Dtype* mask, unsigned int* count) {  
   const int NUM_THREADS = 512;  
   __shared__ unsigned int tcount [2*NUM_THREADS];   
   unsigned int t = threadIdx.x; 
   unsigned int s = 2 * blockIdx.x * NUM_THREADS;
   tcount[t] = 0;
-  if (s+t < n && mask[s+t]!=0){
+  if (s + t < n && mask[s+t] != 0) {
     tcount[t] = 1;
   }
   tcount[t+NUM_THREADS] = 0;
-  if (s+t+NUM_THREADS < n && mask[s+t+NUM_THREADS]!=0){
+  if (s + t + NUM_THREADS < n && mask[s+t+NUM_THREADS] != 0){
     tcount[t+NUM_THREADS] = 1;
   }
   __syncthreads(); 
   for(unsigned int stride = NUM_THREADS; stride >= 1; stride >>= 1) {
-    if (t < stride ){
+    if (t < stride) {
       tcount[t] += tcount[t+stride];
     }
     __syncthreads();  
   }
-  if (t == 0){
+  if (t == 0) {
     count[blockIdx.x] = tcount[0]; 
   }     
 }
@@ -84,10 +92,11 @@ template <typename Dtype>
 __global__ void CCMaskCalc(const int n, const Dtype* wb,
     Dtype* mask, Dtype mu, Dtype std, Dtype r) {
   CUDA_KERNEL_LOOP(index, n) {
-    if (mask[index]==1 && fabs(wb[index])<=0.9*max(mu+r*std,Dtype(0))) 
+    if (mask[index] == 1 && fabs(wb[index]) <= 0.9*max(mu+r*std,Dtype(0))) {
       mask[index] = 0;
-    else if (mask[index]==0 && fabs(wb[index])>1.1*max(mu+r*std,Dtype(0)))
+    } else if (mask[index] == 0 && fabs(wb[index]) > 1.1*max(mu+r*std,Dtype(0))) {
       mask[index] = 1;
+    }
   }
 }
 
@@ -100,10 +109,14 @@ __global__ void CCMaskApply(const int n, const Dtype* wb,
 }
 
 template <typename Dtype>
-void CCMomentCalc(const int n, const Dtype* wb, const Dtype* mask, Dtype* mu, Dtype* std, unsigned int* ncount){ 
+void CCMomentCalc(const int n, const Dtype* wb, const Dtype* mask, Dtype* mu, Dtype* std, unsigned int* ncount) { 
   const unsigned int NUM_THREADS = 512;
-  Dtype* pmu_g; Dtype* pstd_g; unsigned int* pncount_g;
-  Dtype* pmu_c; Dtype* pstd_c; unsigned int* pncount_c;
+  Dtype* pmu_g;
+  Dtype* pstd_g;
+  unsigned int* pncount_g;
+  Dtype* pmu_c;
+  Dtype* pstd_c;
+  unsigned int* pncount_c;
   int num_p = (n+(NUM_THREADS<<1)-1)/(NUM_THREADS<<1);  
   cudaMalloc(&pmu_g, sizeof(Dtype)  * num_p);
   cudaMalloc(&pstd_g, sizeof(Dtype) * num_p);
@@ -117,14 +130,20 @@ void CCMomentCalc(const int n, const Dtype* wb, const Dtype* mask, Dtype* mu, Dt
   cudaMemcpy(pstd_c, pstd_g, sizeof(Dtype) * num_p, cudaMemcpyDeviceToHost);
   cudaMemcpy(pncount_c, pncount_g, sizeof(unsigned int) * num_p, cudaMemcpyDeviceToHost);      
   for (int i = 0; i < num_p; i++) {
-    *mu += pmu_c[i];*std += pstd_c[i];*ncount += pncount_c[i];
+    *mu += pmu_c[i];
+    *std += pstd_c[i];
+    *ncount += pncount_c[i];
   }       
-  cudaFree(pmu_g);cudaFree(pstd_g);cudaFree(pncount_g);
-  free(pmu_c);free(pstd_c);free(pncount_c);
+  cudaFree(pmu_g);
+  cudaFree(pstd_g);
+  cudaFree(pncount_g);
+  free(pmu_c);
+  free(pstd_c);
+  free(pncount_c);
 }
 
 template <typename Dtype>
-void CCNZeroCalc(const int n, const Dtype* mask, unsigned int* ncount ){  
+void CCNZeroCalc(const int n, const Dtype* mask, unsigned int* ncount) {  
   const unsigned int NUM_THREADS = 512;
   unsigned int* pncount_g;
   unsigned int* pncount_c;
@@ -143,48 +162,49 @@ void CCNZeroCalc(const int n, const Dtype* mask, unsigned int* ncount ){
 
 template <typename Dtype>
 void CConvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {  
-
+      const vector<Blob<Dtype>*>& top) {
   const Dtype* weight = this->blobs_[0]->mutable_gpu_data();  
   Dtype* weightMask = this->blobs_[2]->mutable_gpu_data();
   Dtype* weightTmp = this->weight_tmp_.mutable_gpu_data(); 
   const Dtype* bias = NULL;
   Dtype* biasMask = NULL;
   Dtype* biasTmp = NULL;   
-  if (this->bias_term_) {  
+  if (this->bias_term_) {
     bias = this->blobs_[1]->mutable_gpu_data();   
     biasMask = this->blobs_[3]->mutable_gpu_data();
     biasTmp = this->bias_tmp_.mutable_gpu_data();
   }
   
   if (this->phase_ == TRAIN){
-		// Calculate the mean and standard deviation of learnable parameters 		
-    if (this->std==0 && this->iter_==0){
+    // Calculate the mean and standard deviation of learnable parameters 		
+    if (this->std == 0 && this->iter_ == 0) {
       unsigned int ncount = 0;
       CCMomentCalc(this->blobs_[0]->count(), weight, weightMask, &mu, &std, &ncount);
       if (this->bias_term_) {  
         CCMomentCalc(this->blobs_[1]->count(), bias, biasMask, &mu, &std, &ncount); 
       }     
-      this->mu /= ncount; this->std -= ncount*mu*mu; 
-      this->std /= ncount; this->std = sqrt(std);  
-      LOG(INFO)<<mu<<"  "<<std<<"  "<<ncount<<"\n";                    
+      this->mu /= ncount;
+      this->std -= ncount*mu*mu;
+      this->std /= ncount;
+      this->std = sqrt(std);  
+      LOG(INFO) << mu << "  " << std << "  " << ncount;
     }
 		
-		// Demonstrate the sparsity of compressed convolutional layer
-		/********************************************************/
-		/*if(this->iter_%1000==0){
-			unsigned int ncount = 0;
-			CCNZeroCalc(this->blobs_[0]->count(), weightMask, &ncount);
-			if (this->bias_term_) {  
-				CCNZeroCalc(this->blobs_[1]->count(), biasMask, &ncount);   
-			}
-			LOG(INFO)<<ncount<<"\n";  			
-		}*/	
-		/********************************************************/
+    // Demonstrate the sparsity of compressed convolutional layer
+    /********************************************************/
+    if(this->iter_ % 1000 == 0) {
+      unsigned int ncount = 0;
+      CCNZeroCalc(this->blobs_[0]->count(), weightMask, &ncount);
+      if (this->bias_term_) {
+        CCNZeroCalc(this->blobs_[1]->count(), biasMask, &ncount);   
+      }
+      LOG(INFO) << ncount;  			
+    }	
+    /********************************************************/
 		
-		// Calculate the weight mask and bias mask with probability
+    // Calculate the weight mask and bias mask with probability
     Dtype r = static_cast<Dtype>(rand())/static_cast<Dtype>(RAND_MAX);
-    if (pow(1+(this->gamma)*(this->iter_),-(this->power))>r && (this->iter_)<(this->iter_stop_)) { 
+    if (pow(1+(this->gamma)*(this->iter_),-(this->power)) > r && (this->iter_) < (this->iter_stop_)) {
       CCMaskCalc<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[0]->count()),
         CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[0]->count(), weight, 
         weightMask, this->mu, this->std, this->crate);
@@ -193,11 +213,11 @@ void CConvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
         CCMaskCalc<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[1]->count()),
           CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[1]->count(), bias, 
           biasMask, this->mu, this->std, this->crate);
-        CUDA_POST_KERNEL_CHECK; 
+        CUDA_POST_KERNEL_CHECK;
       }
-    }    
-  }   
- 
+    }
+  }
+
   // Calculate the current (masked) weight and bias
   CCMaskApply<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[0]->count()),
     CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[0]->count(), weight, weightMask, weightTmp);
@@ -208,7 +228,7 @@ void CConvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     CUDA_POST_KERNEL_CHECK;  
   }
       
-	// Forward calculation with (masked) weight and bias 
+  // Forward calculation with (masked) weight and bias 
   for (int i = 0; i < bottom.size(); ++i) {
     const Dtype* bottom_data = bottom[i]->gpu_data();
     Dtype* top_data = top[i]->mutable_gpu_data();
@@ -225,14 +245,14 @@ void CConvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void CConvolutionLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-  const Dtype* weightTmp = this->weight_tmp_.gpu_data();  	
-	const Dtype* weightMask = this->blobs_[2]->gpu_data();
-	Dtype* weight_diff = this->blobs_[0]->mutable_gpu_diff();  	
-  for (int i = 0; i < top.size(); ++i) {    
+  const Dtype* weightTmp = this->weight_tmp_.gpu_data();
+  const Dtype* weightMask = this->blobs_[2]->gpu_data();
+  Dtype* weight_diff = this->blobs_[0]->mutable_gpu_diff();
+  for (int i = 0; i < top.size(); ++i) {
     const Dtype* top_diff = top[i]->gpu_diff();
     // Bias gradient, if necessary.
     if (this->bias_term_ && this->param_propagate_down_[1]) {
-			const Dtype* biasMask = this->blobs_[3]->gpu_data();
+      const Dtype* biasMask = this->blobs_[3]->gpu_data();
       Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();			
       CCMaskApply<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[3]->count()),
         CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[3]->count(), bias_diff, biasMask, bias_diff);
--- src/caffe/layers/compress_inner_product_layer.cu
+++ src/caffe/layers/compress_inner_product_layer.cu
@@ -20,23 +20,31 @@ __global__ void CCMomentCollect(const int n, const Dtype* wb, const Dtype* mask,
   __shared__ unsigned int tcount [2*NUM_THREADS];   
   unsigned int t = threadIdx.x; 
   unsigned int s = 2 * blockIdx.x * NUM_THREADS;
-  if (s+t < n){
+  if (s + t < n) {
     param[t] = fabs(mask[s+t]*wb[s+t]);
     param[t+2*NUM_THREADS] = mask[s+t]*wb[s+t]*wb[s+t];
-    if(mask[s+t]*wb[s+t]!=0) tcount[t] = 1;
-    else tcount[t] = 0;
+    if (mask[s+t] * wb[s+t] != 0) {
+      tcount[t] = 1;
+    } else {
+      tcount[t] = 0;
+    }
+  } else {
+    param[t] = 0;
+    param[t+2*NUM_THREADS] = 0;
+    tcount[t] = 0;
   }
-  else{
-    param[t] = 0;param[t+2*NUM_THREADS] = 0;tcount[t] = 0;
-  }
-  if (s+t+NUM_THREADS < n){
+  if (s + t + NUM_THREADS < n) {
     param[t+NUM_THREADS] = fabs(mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS]);
     param[t+3*NUM_THREADS] = mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS];
-    if(mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS]!=0) tcount[t+NUM_THREADS] = 1;
-    else tcount[t+NUM_THREADS] = 0;
-  }
-  else{
-    param[t+NUM_THREADS] = 0;param[t+3*NUM_THREADS] = 0;tcount[t+NUM_THREADS] = 0;  
+    if (mask[s+t+NUM_THREADS]*wb[s+t+NUM_THREADS] != 0) {
+      tcount[t+NUM_THREADS] = 1;
+    } else {
+      tcount[t+NUM_THREADS] = 0;
+    }
+  } else {
+    param[t+NUM_THREADS] = 0;
+    param[t+3*NUM_THREADS] = 0;
+    tcount[t+NUM_THREADS] = 0;  
   }
   __syncthreads(); 
   for(unsigned int stride = NUM_THREADS; stride >= 1; stride >>= 1) {
@@ -62,21 +70,21 @@ __global__ void CCNzeroCollect(const int n, const Dtype* mask, unsigned int* cou
   unsigned int t = threadIdx.x; 
   unsigned int s = 2 * blockIdx.x * NUM_THREADS;
   tcount[t] = 0;
-  if (s+t < n && mask[s+t]!=0){
+  if (s + t < n && mask[s+t] != 0) {
     tcount[t] = 1;
   }
   tcount[t+NUM_THREADS] = 0;
-  if (s+t+NUM_THREADS < n && mask[s+t+NUM_THREADS]!=0){
+  if (s + t + NUM_THREADS < n && mask[s+t+NUM_THREADS] != 0) {
     tcount[t+NUM_THREADS] = 1;
   }
   __syncthreads(); 
-  for(unsigned int stride = NUM_THREADS; stride >= 1; stride >>= 1) {
-    if (t < stride ){
+  for (unsigned int stride = NUM_THREADS; stride >= 1; stride >>= 1) {
+    if (t < stride ) {
       tcount[t] += tcount[t+stride];
     }
     __syncthreads();  
   }
-  if (t == 0){
+  if (t == 0) {
     count[blockIdx.x] = tcount[0]; 
   }     
 }
@@ -85,10 +93,11 @@ template <typename Dtype>
 __global__ void CCMaskCalc(const int n, const Dtype* wb,
     Dtype* mask, Dtype mu, Dtype std, Dtype r) {
   CUDA_KERNEL_LOOP(index, n) {
-    if (mask[index]==1 && fabs(wb[index])<=0.9*max(mu+r*std,Dtype(0))) 
+    if (mask[index] == 1 && fabs(wb[index]) <= 0.9*max(mu+r*std,Dtype(0))) {
       mask[index] = 0;
-    else if (mask[index]==0 && fabs(wb[index])>1.1*max(mu+r*std,Dtype(0)))
+    } else if (mask[index] == 0 && fabs(wb[index]) > 1.1*max(mu+r*std,Dtype(0))) {
       mask[index] = 1;
+    }
   }
 }
 
@@ -103,8 +112,12 @@ __global__ void CCMaskApply(const int n, const Dtype* wb,
 template <typename Dtype>
 void CCMomentCalc(const int n, const Dtype* wb, const Dtype* mask, Dtype* mu, Dtype* std, unsigned int* ncount){ 
   const unsigned int NUM_THREADS = 512;
-  Dtype* pmu_g; Dtype* pstd_g; unsigned int* pncount_g;
-  Dtype* pmu_c; Dtype* pstd_c; unsigned int* pncount_c;
+  Dtype* pmu_g;
+  Dtype* pstd_g;
+  unsigned int* pncount_g;
+  Dtype* pmu_c;
+  Dtype* pstd_c;
+  unsigned int* pncount_c;
   int num_p = (n+(NUM_THREADS<<1)-1)/(NUM_THREADS<<1);  
   cudaMalloc(&pmu_g, sizeof(Dtype)  * num_p);
   cudaMalloc(&pstd_g, sizeof(Dtype) * num_p);
@@ -118,10 +131,16 @@ void CCMomentCalc(const int n, const Dtype* wb, const Dtype* mask, Dtype* mu, Dt
   cudaMemcpy(pstd_c, pstd_g, sizeof(Dtype) * num_p, cudaMemcpyDeviceToHost);
   cudaMemcpy(pncount_c, pncount_g, sizeof(unsigned int) * num_p, cudaMemcpyDeviceToHost);      
   for (int i = 0; i < num_p; i++) {
-    *mu += pmu_c[i];*std += pstd_c[i];*ncount += pncount_c[i];
+    *mu += pmu_c[i];
+    *std += pstd_c[i];
+    *ncount += pncount_c[i];
   }       
-  cudaFree(pmu_g);cudaFree(pstd_g);cudaFree(pncount_g);
-  free(pmu_c);free(pstd_c);free(pncount_c);
+  cudaFree(pmu_g);
+  cudaFree(pstd_g);
+  cudaFree(pncount_g);
+  free(pmu_c);
+  free(pstd_c);
+  free(pncount_c);
 }
 
 template <typename Dtype>
@@ -145,7 +164,6 @@ void CCNZeroCalc(const int n, const Dtype* mask, unsigned int* ncount ){
 template <typename Dtype>
 void CInnerProductLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {    
-
   const Dtype* weight = this->blobs_[0]->mutable_gpu_data();  
   Dtype* weightMask = this->blobs_[2]->mutable_gpu_data();
   Dtype* weightTmp = this->weight_tmp_.mutable_gpu_data();  
@@ -158,45 +176,47 @@ void CInnerProductLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     biasTmp = this->bias_tmp_.mutable_gpu_data();
   }   
     
-  if (this->phase_ == TRAIN){
-		// Calculate the mean and standard deviation of learnable parameters 		
-    if (this->std==0 && this->iter_==0){
+  if (this->phase_ == TRAIN) {
+    // Calculate the mean and standard deviation of learnable parameters 		
+    if (this->std == 0 && this->iter_ == 0) {
       unsigned int ncount = 0;
       CCMomentCalc(this->blobs_[0]->count(), weight, weightMask, &mu, &std, &ncount);
-      if (this->bias_term_) {  
-        CCMomentCalc(this->blobs_[1]->count(), bias, biasMask, &mu, &std, &ncount); 
-      }     
-      this->mu /= ncount; this->std -= ncount*mu*mu; 
-      this->std /= ncount; this->std = sqrt(std);  
-      LOG(INFO)<<mu<<"  "<<std<<"  "<<ncount<<"\n";                    
+      if (this->bias_term_) {
+        CCMomentCalc(this->blobs_[1]->count(), bias, biasMask, &mu, &std, &ncount);
+      }
+      this->mu /= ncount;
+      this->std -= ncount*mu*mu;
+      this->std /= ncount;
+      this->std = sqrt(std);
+      LOG(INFO) << mu << "  " << std << "  " << ncount;
     }
+
+    // Demonstrate the sparsity of compressed fully-connected layer
+    /********************************************************/
+    if (this->iter_ % 100 == 0) {
+      unsigned int ncount = 0;
+      CCNZeroCalc(this->blobs_[0]->count(), weightMask, &ncount);
+      if (this->bias_term_) {
+        CCNZeroCalc(this->blobs_[1]->count(), biasMask, &ncount);
+      }
+      LOG(INFO) << ncount;
+    }	
+    /********************************************************/		
 		
-		// Demonstrate the sparsity of compressed fully-connected layer
-		/********************************************************/
-		/*if(this->iter_%100==0){
-			unsigned int ncount = 0;
-			CCNZeroCalc(this->blobs_[0]->count(), weightMask, &ncount);
-			if (this->bias_term_) {  
-				CCNZeroCalc(this->blobs_[1]->count(), biasMask, &ncount);   
-			}
-			LOG(INFO)<<ncount<<"\n";  			
-		}*/	
-		/********************************************************/		
-		
-		// Calculate the weight mask and bias mask with probability
+    // Calculate the weight mask and bias mask with probability
     Dtype r = static_cast<Dtype>(rand())/static_cast<Dtype>(RAND_MAX);
-    if (pow(1+(this->gamma)*(this->iter_),-(this->power))>r && (this->iter_)<(this->iter_stop_)) { 
+    if (pow(1+(this->gamma)*(this->iter_),-(this->power)) > r && (this->iter_) < (this->iter_stop_)) {
       CCMaskCalc<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[0]->count()),
         CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[0]->count(), weight, weightMask, this->mu, this->std, this->crate);
-      CUDA_POST_KERNEL_CHECK;    
-      if (this->bias_term_) {  
+      CUDA_POST_KERNEL_CHECK;
+      if (this->bias_term_) {
         CCMaskCalc<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[1]->count()),
           CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[1]->count(), bias, biasMask, this->mu, this->std, this->crate);
         CUDA_POST_KERNEL_CHECK;  
-      }    
+      }
     }
-  }  
-  
+  }
+
   // Calculate the current (masked) weight and bias
   CCMaskApply<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[0]->count()),
     CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[0]->count(), weight, weightMask, weightTmp);
@@ -207,22 +227,24 @@ void CInnerProductLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     CUDA_POST_KERNEL_CHECK;  
   } 
    
-	// Forward calculation with (masked) weight and bias 
+  // Forward calculation with (masked) weight and bias 
   const Dtype* bottom_data = bottom[0]->gpu_data();
   Dtype* top_data = top[0]->mutable_gpu_data();
   if (M_ == 1) {
     caffe_gpu_gemv<Dtype>(CblasNoTrans, N_, K_, (Dtype)1.,
                          weightTmp, bottom_data, (Dtype)0., top_data);
-    if (this->bias_term_)
+    if (this->bias_term_) {
       caffe_gpu_axpy<Dtype>(N_, bias_multiplier_.cpu_data()[0],
                             biasTmp, top_data);
+    }
   } else {
     caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasTrans, M_, N_, K_, (Dtype)1.,
                           bottom_data, weightTmp, (Dtype)0., top_data);
-    if (this->bias_term_)
+    if (this->bias_term_) {
       caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, N_, 1, (Dtype)1.,
                             bias_multiplier_.gpu_data(),
                             biasTmp, (Dtype)1., top_data);
+    }
   }
 }
 
@@ -232,18 +254,18 @@ void CInnerProductLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     const vector<Blob<Dtype>*>& bottom) {
   const Dtype* top_diff = top[0]->gpu_diff();
   if (this->param_propagate_down_[0]) {
-		const Dtype* weightMask = this->blobs_[2]->gpu_data();
+    const Dtype* weightMask = this->blobs_[2]->gpu_data();
     Dtype* weight_diff = this->blobs_[0]->mutable_gpu_diff();
     const Dtype* bottom_data = bottom[0]->gpu_data();
     // Gradient with respect to weight
-		CCMaskApply<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[2]->count()),
-      CAFFE_CUDA_NUM_THREADS>>>( this->blobs_[2]->count(), weight_diff, weightMask, weight_diff);
+    CCMaskApply<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[2]->count()),
+    CAFFE_CUDA_NUM_THREADS>>>(this->blobs_[2]->count(), weight_diff, weightMask, weight_diff);
     CUDA_POST_KERNEL_CHECK; 
     caffe_gpu_gemm<Dtype>(CblasTrans, CblasNoTrans, N_, K_, M_, (Dtype)1.,
         top_diff, bottom_data, (Dtype)1., weight_diff);
   }
   if (bias_term_ && this->param_propagate_down_[1]) {
-		const Dtype* biasMask = this->blobs_[3]->gpu_data();
+    const Dtype* biasMask = this->blobs_[3]->gpu_data();
     Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();
     // Gradient with respect to bias
     CCMaskApply<Dtype><<<CAFFE_GET_BLOCKS(this->blobs_[3]->count()),
@@ -253,7 +275,7 @@ void CInnerProductLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
         bias_multiplier_.gpu_data(), (Dtype)1.,bias_diff);
   }	
   if (propagate_down[0]) {
-		const Dtype* weightTmp = this->weight_tmp_.gpu_data();        
+    const Dtype* weightTmp = this->weight_tmp_.gpu_data();        
     // Gradient with respect to bottom data
     caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, K_, N_, (Dtype)1.,
         top_diff, weightTmp, (Dtype)0.,
